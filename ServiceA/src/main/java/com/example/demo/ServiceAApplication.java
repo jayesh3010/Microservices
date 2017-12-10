d4aa38d86e8271ae1d38c927cfea731a17b70554
Jayesh <jayesh3010@yahoo.com>
2017-12-10 00:23:12 -0600

    commit

--- ServiceA/src/main/java/com/example/demo/ServiceAApplication.java
+++ ServiceA/src/main/java/com/example/demo/ServiceAApplication.java
@@ -2,11 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ServiceAApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
+	}
+	
+	public String callServiceA()
+	{
+		String str = "This is in ServiceA";
+		System.out.println(str);
+		return str;
 	}
 }
