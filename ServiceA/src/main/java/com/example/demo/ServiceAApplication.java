8731cd5b5d66f344a2bd73538f2d3186b1c4d7ca
Jayesh <jayesh3010@yahoo.com>
2017-12-10 22:38:27 -0600

    commit

--- ServiceA/src/main/java/com/example/demo/ServiceAApplication.java
+++ ServiceA/src/main/java/com/example/demo/ServiceAApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
@@ -13,6 +14,7 @@ public class ServiceAApplication {
 
 	@Autowired
 	Environment environment;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceAApplication.class, args);
@@ -23,6 +25,20 @@ public class ServiceAApplication {
 	{
 		String str = "This is in ServiceA : port : " + environment.getProperty("local.server.port");
 		System.out.println(str);
+		return str;
+	}
+	
+	@RequestMapping("/serviceab")
+	public String callServiceAB()
+	{
+		String str = callServiceA() + "\n";
+		
+		RestTemplate resttemplate= new RestTemplate();
+		
+		str += resttemplate.getForObject("http://localhost:8092/serviceb", String.class);
+		
+		System.out.println(str);
+		
 		return str;
 	}
 }
